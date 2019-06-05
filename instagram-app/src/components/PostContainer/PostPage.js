bac45fff2bbe71b29e627394251845bcea968bf0
LadyKerr <kadiekerr@gmail.com>
2019-06-05 16:22:16 -0400

    data finally shows up on screen!

--- instagram-app/src/components/PostContainer/PostPage.js
+++ instagram-app/src/components/PostContainer/PostPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import dummyData from './dummy-data';
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
+import dummyData from '../../dummy-data';
+import SearchBar from "../SearchBar/SearchBar";
+import PostContainer from "./PostContainer";
 
 
 class PostPage extends React.Component {
